07d35761b04a4e275be58eb9792f1f1ab35dc2f8
gaynor-landmark <g.l.landmark@gmail.com>
2016-02-20 14:49:45 +1300

    reorg of file structure and add browserify and watchify

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var path = require('path')
+var cors = require('cors')
+
+var $ = require('jquery')
+var bodyParser = require('body-parser')
+
+var app = express()
+
+app.use(bodyParser.json());
+
+app.use(cors({
+  origin: 'http://localhost:9965'
+}))
+
+app.set('port', 3000)
+
+//app.use(express.static(path.join(__dirname,'public')))
+
+var server = app.listen(app.get('port'), function(){
+  var port = server.address().port
+  console.log('listening on ' + port)
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
