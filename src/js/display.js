c9e2abb921c08e4dc5f657602d5eb1fc99af1ce5
gaynor-landmark <g.l.landmark@gmail.com>
2016-02-20 22:24:35 +1300

    add simple-color-picker

--- src/js/display.js
+++ src/js/display.js
@@ -1,17 +1,19 @@
 var $ = require('jquery')
+var ColorPicker = require('simple-color-picker');
 
 module.exports = function(){
+  var colorPicker = new ColorPicker();
 
-  $('#colour1').click(function (){
-    console.log('click1')
-    $('#siteTemplate').css("background-color", "orange")
-  })
-  $('#colour2').click(function (){
-    console.log('click1')
+  colorPicker.appendTo(document.querySelector("#picker"))
+  // listener
+  $('#colour1').click(function (e){
+    console.log('click1', e.target.id)
     $('#siteTemplate').css("background-color", "white")
+
+
+
+
+
   })
-  $('#colour3').click(function (){
-    console.log('click1')
-    $('#siteTemplate').css("background-color", "blue")
-  })
+
 }
