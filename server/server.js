5dc183b6cdad04083c0c2d85f246f66ccb5a6eeb
gaynor-landmark <g.l.landmark@gmail.com>
2016-03-02 22:57:07 +1300

    add first pass at POST to db

--- server/server.js
+++ server/server.js
@@ -16,11 +16,23 @@ app.use(cors({
 var knex = require('knex')({
   client: 'sqlite3',
   connection: {
-    filename: '../db/pets.sqlite'
+    filename: '../db/colourschemes.sqlite'
   },
   useNullAsDefault: true
 })
 
+
+app.post('/palettes', function(req, res) {
+  var newId = = uuid.v4()
+  knex('palettes').insert({
+        PaletteID: newId ,
+        PaletteName: req.body.name,
+        UserID: req.body.userId,
+        Colours: req.body.colours
+      }).then(function(resp) {
+        res.send('Saved')
+      })
+})
 
 
 app.set('port', 3000)
