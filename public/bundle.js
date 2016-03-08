e0d03609eb45fef33847f231a4613611c9b8a502
gaynor-landmark <g.l.landmark@gmail.com>
2016-03-08 14:13:54 +1300

    passing back colours on reload

--- public/bundle.js
+++ public/bundle.js
@@ -46240,9 +46240,22 @@ module.exports = function(){
   })
   inspirePalette()
   clearPalette()
- welcome("Guest")
+  // var qstringColours = window.location.href.slice(window.location.href.indexOf('?') + 1)
+  // loadCustomPalette(qstringColours)
 
+ function loadCustomPalette (colourString) {
+  console.log("cload", colourString)
 
+  if (colourString > 0){
+    var arr = colourString.split('|')
+    for (var c = 1; c < 6; c++){
+      myswatch = "#swatch" + c
+      myhex = myswatch.replace("swatch", "hex")
+      document.querySelector(myswatch).style.background =  hex2rgb(arr[c - 1])
+      $(myhex).text('#' + arr[c - 1])
+    }
+  }
+}
 
 function addFromColourLoversPalette(target) {
   var which = target.id
