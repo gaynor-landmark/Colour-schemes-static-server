aa1cb54297dd6758a3bb0074990099ec15129936
gaynor-landmark <g.l.landmark@gmail.com>
2016-03-08 21:46:53 +1300

    fix initial palette load

--- src/js/display.js
+++ src/js/display.js
@@ -22,23 +22,23 @@ module.exports = function(){
   })
   inspirePalette()
   clearPalette()
-
-
- loadCustomPalette()
+  loadCustomPalette()
 
  function loadCustomPalette () {
-
-  var colourString = window.location.href.slice(window.location.href.indexOf('?') + 1)
-  console.log("cload", colourString)
-  if (colourString.length > 0){
-    var arr = colourString.split('|')
-    for (var c = 1; c < 6; c++){
-      myswatch = "#swatch" + c
-      myhex = myswatch.replace("swatch", "hex")
-      document.querySelector(myswatch).style.background =  hex2rgb(arr[c - 1])
-      $(myhex).text('#' + arr[c - 1])
+   console.log("precload", window.location.href.indexOf('?'), colourString)
+   if (window.location.href.indexOf('?') >= 0) {
+      var colourString = window.location.href.slice(window.location.href.indexOf('?') + 1)
+      console.log("cload", colourString)
+      if (colourString.length > 0){
+        var arr = colourString.split('|')
+        for (var c = 1; c < 6; c++){
+          myswatch = "#swatch" + c
+          myhex = myswatch.replace("swatch", "hex")
+          document.querySelector(myswatch).style.background =  hex2rgb(arr[c - 1])
+          $(myhex).text('#' + arr[c - 1])
+        }
+      }
     }
-  }
 }
 
 function addFromColourLoversPalette(target) {
