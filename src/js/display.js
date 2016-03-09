9918460d509957cf544336a9c649057e37d88cb6
gaynor-landmark <g.l.landmark@gmail.com>
2016-03-09 15:34:38 +1300

    add one working test

--- src/js/display.js
+++ src/js/display.js
@@ -2,8 +2,8 @@ var $ = require('jquery')
 var colourlovers = require('colourlovers')
 var ColorPicker = require('simple-color-picker')
 var handlebars = require('handlebars')
-
 var request = require('superagent')
+
 var rgb2hex = require('./rgb2hex.js')
 var hex2rgb = require('./hex2rgb.js')
 var clearPalette = require('./clearPalette.js')
@@ -26,55 +26,6 @@ module.exports = function(){
   clearPalette()
   loadCustomPalette()
   addListeners()
-
- // function addListeners(){
- //   $('#clearButton').click(function(e){clearPalette()})
- //
- //   $('#saveButton').click(function(e){
- //     var paletteJson = {}
- //     paletteJson.Name = $('#palettename').val()
- //     if (!paletteJson.Name.trim()){
- //       alert("Your palette needs a name before it can be saved.")
- //     } else {
- //       coloursString = ""
- //       for (var c = 1; c < 6; c++){
- //         var myswatch = "#swatch" + c
- //         coloursString += (rgb2hex(document.querySelector(myswatch).style.background)) + "|"
- //       }
- //       paletteJson.Colours = coloursString
- //       savePalette(paletteJson)
- //     }
- //   })
- //
- //   for (var c = 1; c < 6; c++) {
- //     var mycolour = "#colour" + c
- //     $(mycolour).click(function (e){
- //       updateElement(document.querySelector(mycolour).style.background)
- //     })
- //   }
- //
- //   // add listeners to custom palette
- //   for (var a = 1; a < 6; a++) {
- //     var myswatch = "#swatch" + a
- //     $(myswatch).click(function(e){addToPalette(e.target.id)})
- //   }
- //   // add listeners to colourlovers palette
- //   for (var b = 1; b < 6; b++) {
- //     var myswatch = "#swatch" + (b * 10)
- //     $(myswatch).click(function(e){addFromColourLoversPalette(e.target)})
- //   }
- //   $('#randomButton').click(function(e){inspirePalette()})
- //
- //   $('#tryButton').click(function(e){
- //     var qString = createQueryString()
- //     window.location = './tryItOut.html' + qString
- //   })
- //
- //   $('#myPalettesButton').click(function(){
- //     appendPalettes()
- //   })
- //
- // }
 
 }
 
--- test/getRequest.js
+++ test/getRequest.js
@@ -1,6 +1,28 @@
 var test = require('tape')
+var tapspec = require('tap-spec')
+var supertest = require('supertest')
+var getrequest = require('../src/js/getRequest.js')
 
-test("returns a palette"){
 
-
+// a palette has been set up in the db
+var expectedPalette =  { UserID : '1',
+      PaletteID : '1',
+      PaletteName : 'test palette',
+      Colour1 : '#A3A948',
+      Colour2 : '#EDB92E',
+      Colour3 : '#F85931',
+      Colour4 : '#CE1836',
+      Colour5 : '#009989'
 }
+
+test("returns the correct palette", function(t){
+  // a palette has been set up in the db
+  console.log("in the test")
+  getrequest('http://localhost:3000/palettes/1', function (res){
+    var actualPalette = res
+      console.log("res", res)
+      t.deepEqual( actualPalette, expectedPalette)
+      t.end()
+  })
+
+})
