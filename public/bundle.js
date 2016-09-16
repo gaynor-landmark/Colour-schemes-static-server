4a9eb300697b1891554ae12a4c1ba1bd201a367d
gaynor-landmark <g.l.landmark@gmail.com>
2016-09-16 22:46:24 +1200

    http to s

--- public/bundle.js
+++ public/bundle.js
@@ -44374,7 +44374,7 @@ var hex2rgb = require('./hex2rgb.js')
 
 module.exports = function(){
   // retrieve a set of the 50 top palettes from colourlovers.com
-  $.getJSON('http://www.colourlovers.com/api/palettes/top?jsonCallback=?&numResults=50', function(data){
+  $.getJSON('https://www.colourlovers.com/api/palettes/top?jsonCallback=?&numResults=50', function(data){
     var clPalette = data[Math.floor(Math.random()*50)]  // choose one randomly to display
     $("#clname").text(clPalette["title"] + ' by ' + clPalette["userName"])
     for (var c = 1; c < 6; c++){
